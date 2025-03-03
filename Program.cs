9f027c5b7d3739bf2ee22e43c0ebe3545812eb9c
marcinruszczyk <ruszmar@gmail.com>
2025-03-03 09:55:03 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //testowy projekt
+        }
+    }
+}
